915bf53aa4143332fa2e3535f7804e823f8e280d
kim <kimemmanuel004@gmail.com>
2022-08-22 15:58:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    console.log(name);
+    
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    console.log(name);
+
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log();
+
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log();
+
+}
+function appendCat(name) {
+    cats.push("Broom"); 
+    
+}
+function prependCat(name) {
+    cats.shift("Arnold");
+}
